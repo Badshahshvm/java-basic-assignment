4e63212cd453859a07da58653c3bf1a66e7bcd5e
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-07-03 18:52:16 +0530

    Update Pattern display.java

--- Pattern display.java
+++ Pattern display.java
@@ -2,9 +2,15 @@
 
 public class PatternJava {
     public static void main(String[] args) {
-        System.out.println("  J    a   v     v  a");
-        System.out.println("   J   a a   v   v  a a");
-        System.out.println("J  J  aaaaa   V V  aaaaa");
-        System.out.println(" JJ  a     a   V  a     a");
+      String[] pattern = {
+            "   J    a   v     v  a",
+            "   J   a a   v   v  a a",
+            "J  J  aaaaa   V V  aaaaa",
+            " JJ  a     a   V  a     a"
+        };
+
+        for (String line : pattern) {
+            System.out.println(line);
+        }
     }
 }
